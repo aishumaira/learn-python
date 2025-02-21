ca24b5f755644c6db94f8c57cc6334283b2e483d
aishumaira <aisyah.humaira@student.alhazen.academy>
2025-02-21 13:49:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from mathematic.square.area import square_area
+from mathematic.circle.area import circle_area
+from mathematic.triangle.area import triangla_area
+
+square_area(25)
+triangla_area(12,8)
+circle_area(7)
