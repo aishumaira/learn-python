f57bfe3a8f2065b336ff59739c55fa6820b191ec
aishumaira <aisyah.humaira@student.alhazen.academy>
2025-04-25 14:17:50 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,8 @@
+from stringcolor import *
+
+print(cs("Selamat Datang", "blue", "white"))
+#                    warna tulisan  warna background
+print(bold("Selamat Datang"))
+print(underline("Selamat Datang"))
+print(underline("Selamat Datang").bold())
+print(cs("Selamat Datang", "blue", "yellow").underline())
