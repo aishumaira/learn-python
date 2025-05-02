e674e684ef480ae5d54057eb13368328d17a7473
aishumaira <aisyah.humaira@student.alhazen.academy>
2025-05-02 13:56:09 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from mathematic.square.area import square_area
-from mathematic.circle.area import circle_area
-from mathematic.triangle.area import triangla_area
+import json
+data = {
+    'firstname' : 'Ahmad',
+    'lastname' : 'Maulana',
+    'age' : 26,
+    'hobbies' : [
+        'swimming',
+        'programming'
+    ]
+}
 
-square_area(25)
-triangla_area(12,8)
-circle_area(7)
+data_json = json.dumps(data)
+print(data_json)
