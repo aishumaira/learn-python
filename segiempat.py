350722433502837cc3348fed3aba784c3c393798
aishumaira <aisyah.humaira@student.alhazen.academy>
2025-05-16 14:25:15 +0700

    Add files via upload

---
+++ segiempat.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+getscreen() #Untuk menampilkan window
+bgcolor("gray")
+pencolor("blue")
+pensize(5)
+shape("turtle")
+speed(1)
+
+for i in range(4):
+    forward(200)
+    left(90)
+
+done() #bikin window tetap terbuka
